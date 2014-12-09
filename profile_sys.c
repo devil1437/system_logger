2165f951f3be4e574aa49768acf655b04daf5d19
devil1437 <r02922029@csie.ntu.edu.tw>
2014-12-09 21:36:30 +0800

    Fix the bug when opening(closing) CPU.

--- profile_sys.c
+++ profile_sys.c
@@ -5,14 +5,16 @@
 #include <string.h>
 #include <sys/select.h>
 #include <termios.h>
+#include <time.h>
 
-//#include <time.h>
+//#define ENABLE_NETWORK
+#define DEBUG
 
 #define CPU_NUM	4
-#define SAMPLE_UNIT_UTIME 200000  /* 10Hz */
-//#define SAMPLE_UNIT_UTIME 0 
-#define SAMPLE_UNIT_SECOND 0
-//#define SAMPLE_UNIT_SECOND 3
+#define DEFAULT_SAMPLE_UNIT_UTIME 100000  // Unit: us
+#define DEFAULT_SAMPLE_UNIT_SECOND 0
+#define DEFAULT_UID 10222
+#define DEFAULT_OUTPUT_NAME "/sdcard/systemLogger/output.csv"
 
 #define CONFIG_HZ 100
 #define BUFF_SIZE 513
@@ -49,10 +51,10 @@ char outfile_name[129];
 int curFreq, maxFreq;
 int cpu_on;
 int uid;
-double util0, util1, util2, util3; 
-unsigned long long lastwork0,lastwork1,lastwork2,lastwork3, workload0, workload1, workload2, workload3;
-unsigned long long curwork0, curwork1, curwork2, curwork3;
-unsigned long long idle0, idle1, idle2, idle3, lastidle0, lastidle1, lastidle2, lastidle3;
+double util[CPU_NUM]; 
+unsigned long long lastwork[CPU_NUM], workload[CPU_NUM];
+unsigned long long curwork[CPU_NUM];
+unsigned long long idle[CPU_NUM], lastidle[CPU_NUM];
 unsigned long long processR, ctxt, lastCtxt;
 
 
@@ -124,9 +126,20 @@ void getCPUMaxFreq(int cpu_num){
 void parse(){
 	char buff[128];
 	int i;
+	struct timeval tv;
+	time_t nowtime;
+	struct tm *nowtm;
+	char timeStr[256];
 	/////////////////////////////////////////////
 	
 	FILE *fp_out = fopen(outfile_name, "a");
+
+	/* get current time */
+	gettimeofday(&tv,NULL);
+	nowtime = tv.tv_sec;
+	nowtm = localtime(&nowtime);
+	strftime(buff, sizeof(buff), "%Y-%m-%d-%H:%M:%S", nowtm);
+	snprintf(timeStr, sizeof(timeStr), "%s.%06ld", buff, tv.tv_usec);
 
 	/* profile cpu frequency */
 	FILE *fp_freq = fopen("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", "r");
@@ -147,40 +160,24 @@ void parse(){
 	}
 		
 	/* profile cpu util. */
-	workload0 = workload1 = workload2 = workload3 = 0;
 	FILE *fp = fopen("/proc/stat","r");
 	while(fgets(buff, 128, fp))
 	{
-		if(strstr(buff, "cpu0 "))
-		{   
-			sscanf(buff, "cpu0 %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &CPUInfo[0][0], &CPUInfo[0][1], &CPUInfo[0][2], &CPUInfo[0][3], &CPUInfo[0][4], &CPUInfo[0][5], &CPUInfo[0][6], &CPUInfo[0][7], &CPUInfo[0][8], &CPUInfo[0][9]); // time(unit: jiffies) spent of all cpus for: user nice system idle iowait irq softirq stead guest
-			workload0 = CPUInfo[0][0]+CPUInfo[0][1]+CPUInfo[0][2]+CPUInfo[0][4]+CPUInfo[0][5]+CPUInfo[0][6]+CPUInfo[0][7];
-			idle0 = CPUInfo[0][3] - lastidle0;
-			curwork0 = workload0 - lastwork0;
-			util0 = (double)curwork0 /(double)(curwork0+idle0);
-		}else if(strstr(buff, "cpu1 "))
-		{   
-			sscanf(buff, "cpu1 %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &CPUInfo[1][0], &CPUInfo[1][1], &CPUInfo[1][2], &CPUInfo[1][3], &CPUInfo[1][4], &CPUInfo[1][5], &CPUInfo[1][6], &CPUInfo[1][7], &CPUInfo[1][8], &CPUInfo[1][9]); // time(unit: jiffies) spent of all cpus for: user nice system idle iowait irq softirq stead guest
-			workload1 = CPUInfo[1][0]+CPUInfo[1][1]+CPUInfo[1][2]+CPUInfo[1][4]+CPUInfo[1][5]+CPUInfo[1][6]+CPUInfo[1][7];
-			idle1 = CPUInfo[1][3] - lastidle1;
-			curwork1 = workload1 - lastwork1;
-			util1 = (double)curwork1 /(double)(curwork1+idle1);
-		}else if(strstr(buff, "cpu2 "))
-		{   
-			sscanf(buff, "cpu2 %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &CPUInfo[2][0], &CPUInfo[2][1], &CPUInfo[2][2], &CPUInfo[2][3], &CPUInfo[2][4], &CPUInfo[2][5], &CPUInfo[2][6], &CPUInfo[2][7], &CPUInfo[2][8], &CPUInfo[2][9]); // time(unit: jiffies) spent of all cpus for: user nice system idle iowait irq softirq stead guest
-			workload2 = CPUInfo[2][0]+CPUInfo[2][1]+CPUInfo[2][2]+CPUInfo[2][4]+CPUInfo[2][5]+CPUInfo[2][6]+CPUInfo[2][7];
-			idle2 = CPUInfo[2][3] - lastidle2;
-			curwork2 = workload2 - lastwork2;
-			util2 = (double)curwork2 /(double)(curwork2+idle2);
-		}else if(strstr(buff, "cpu3 "))
-		{   
-			sscanf(buff, "cpu3 %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", &CPUInfo[3][0], &CPUInfo[3][1], &CPUInfo[3][2], &CPUInfo[3][3], &CPUInfo[3][4], &CPUInfo[3][5], &CPUInfo[3][6], &CPUInfo[3][7], &CPUInfo[3][8], &CPUInfo[3][9]); // time(unit: jiffies) spent of all cpus for: user nice system idle iowait irq softirq stead guest
-			workload3 = CPUInfo[3][0]+CPUInfo[3][1]+CPUInfo[3][2]+CPUInfo[3][4]+CPUInfo[3][5]+CPUInfo[3][6]+CPUInfo[3][7];
-			idle3 = CPUInfo[3][3] - lastidle3;
-			curwork3 = workload3 - lastwork3;
-			util3 = (double)curwork3 /(double)(curwork3+idle3);
+		for(i = 0; i < CPU_NUM; i++)
+		{
+			char findStr[128];
+			sprintf(findStr, "cpu%d ", i);
+			if(strstr(buff, findStr))
+			{   
+				// time(unit: jiffies) spent of all cpus for: user nice system idle iowait irq softirq stead guest
+				sscanf(buff, "%s%llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", findStr, &CPUInfo[i][0], &CPUInfo[i][1], &CPUInfo[i][2], &CPUInfo[i][3], &CPUInfo[i][4], &CPUInfo[i][5], &CPUInfo[i][6], &CPUInfo[i][7], &CPUInfo[i][8], &CPUInfo[i][9]); 
+				workload[i] = CPUInfo[i][0]+CPUInfo[i][1]+CPUInfo[i][2]+CPUInfo[i][4]+CPUInfo[i][5]+CPUInfo[i][6]+CPUInfo[i][7];
+				idle[i] = CPUInfo[i][3] - lastidle[i];
+			}		
+			curwork[i] = workload[i] - lastwork[i];
+			// In order to avoid div0 fault, we need to check the value.
+			util[i] = curwork == 0 ? 0 : (double)curwork[i] /(double)(curwork[i]+idle[i]);
 		}
-
 		if(strstr(buff, "procs_running"))
 		{
 			sscanf(buff, "procs_running %llu", &processR); // # of processes running
@@ -192,23 +189,13 @@ void parse(){
 	}
 	fclose(fp);
 
-	if(workload0 == 0) curwork0 = 0;
-	if(workload1 == 0) curwork1 = 0;
-	if(workload2 == 0) curwork2 = 0;
-	if(workload3 == 0) curwork3 = 0;
-	
-	/* normalization */
-	util0 = util0 * curFreq; // / maxFreq;
-	util1 = util1 * curFreq; // / maxFreq;
-	util2 = util2 * curFreq; // / maxFreq;
-	util3 = util3 * curFreq; // / maxFreq;
-	
 	/* brightness */
 	FILE *fp_bl = fopen("/sys/class/backlight/panel/brightness","r");
 	int brightness;
 	fgets(buff, 128, fp_bl);
 	sscanf(buff,"%d",&brightness);
 
+#ifdef ENABLE_NETWORK
 	/* network */
 	char tcp_send[100];
 	char tcp_rcv[100];
@@ -221,25 +208,33 @@ void parse(){
 	sscanf(buff,"%d",&snd);
 	fgets(buff, 128, fp_rcv);
 	sscanf(buff,"%d",&rcv);
+#else
+	int snd = 0,rcv = 0;
+#endif
 
-	
 	/* output */
-	fprintf(fp_out, "%d,%d,%llu,%.2f,%llu,%.2f,%llu,%.2f,%llu,%.2f,%llu,%llu,%d,%d,%d\n", cpu_on, curFreq, curwork0, util0, curwork1, util1, curwork2, util2, curwork3, util3, processR, ctxt-lastCtxt,brightness,snd,rcv);
+	fprintf(fp_out, "%s,%d,%d", timeStr, cpu_on, curFreq);
+	for(i = 0; i < CPU_NUM; i++){
+		fprintf(fp_out, ",%llu,%.3f", curwork[i], util[i]);
+	}
+	fprintf(fp_out, ",%llu,%llu,%d,%d,%d\n", processR, ctxt-lastCtxt,brightness,snd,rcv);
 	fclose(fp_out);
-	//printf("%d, %llu, %.2f, %llu, %llu", 	curFreq, curwork0, util0, processR, ctxt-lastCtxt);
+#ifdef DEBUG	
+	printf("%s,%d,%d", timeStr, cpu_on, curFreq);
+	for(i = 0; i < CPU_NUM; i++){
+		printf(",%llu,%.3f", curwork[i], util[i]);
+	}
+	printf(",%llu,%llu,%d,%d,%d\n", processR, ctxt-lastCtxt,brightness,snd,rcv);
+#endif
+	
 	
 	/* update data */
-	lastwork0 = workload0;
-	lastwork1 = workload1;
-	lastwork2 = workload2;
-	lastwork3 = workload3;
-	lastidle0 = CPUInfo[0][3];
-	lastidle1 = CPUInfo[1][3];
-	lastidle2 = CPUInfo[2][3];
-	lastidle3 = CPUInfo[3][3];
+	for(i = 0; i < CPU_NUM; i++){
+		lastwork[i] = workload[i];
+		lastidle[i] = CPUInfo[i][3];
+	}
 	lastCtxt = ctxt;
 
-	
 	if(stopFlag == 1)
 	{
 		tick.it_value.tv_sec = 0;
@@ -256,17 +251,24 @@ void parse(){
 
 int main(int argc, char **argv)
 {
-	int res;
+	int res, i;
 	char c;
 	struct timezone timez;
 	int sec, usec;
 //#ifdef TARGET_ONLY
 	//target_pid = 0;
-	if(argc < 5){
+	if(argc == 1){
+		sec = DEFAULT_SAMPLE_UNIT_SECOND;
+		usec = DEFAULT_SAMPLE_UNIT_UTIME;
+		uid = DEFAULT_UID;
+		strcpy(outfile_name, DEFAULT_OUTPUT_NAME);
+		tick.it_value.tv_sec = tick.it_interval.tv_sec = sec;
+		tick.it_value.tv_usec = tick.it_interval.tv_usec = usec;
+	} else if(argc < 5) {
 		printf("please enter sample period\n");
 		printf("usage: %s [sec] [usec] [output file] [uid]\n", argv[0]);
 		return 0;
-	}else{
+	} else {
 		sscanf(argv[1], "%d", &sec);
 		sscanf(argv[2], "%d", &usec);
 		sscanf(argv[4], "%d", &uid);
@@ -276,15 +278,21 @@ int main(int argc, char **argv)
 	}
 //#endif
 	/* initialize */
-	lastwork0 = lastwork1 = workload0 = workload1 = 0;
-	lastwork2 = lastwork3 = workload2 = workload3 = 0;
-	idle0=idle1=lastidle0 =lastidle1 =0;
-	idle2=idle3=lastidle2 =lastidle3 =0;
+	for(i = 0; i < CPU_NUM; i++){
+		lastwork[i] = 0;
+		workload[i] = 0;
+		idle[i] = 0;
+		lastidle[i] = 0;
+	}
 	ctxt = lastCtxt = 0;
 	
 	
 	FILE *fp_out = fopen(outfile_name, "w");
-	fprintf(fp_out, "cpu_on,curFreq,work_cycles0,util0,work_cycles1,util1,work_cycles2,util2,work_cycles3,util3,proc_running,ctxt,brightness,snd,rcv\n");
+	fprintf(fp_out, "time,cpu_on,curFreq");
+	for(i = 0; i < CPU_NUM; i++){
+		fprintf(fp_out, ",work_cycles%d,util%d", i, i);
+	}
+	fprintf(fp_out, ",proc_running,ctxt,brightness,snd,rcv\n");
 	fclose(fp_out);
 /*	
 	FILE *fp_thout = fopen("/sdcard/cuckoo_thprof.csv", "a");
